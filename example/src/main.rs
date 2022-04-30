016001c351feb29edc3b3505c3409de4cfd51167
szuwgh <895340293@qq.com>
2022-04-30 11:24:35 +0800

    revise example

--- example/src/main.rs
+++ example/src/main.rs
@@ -9,9 +9,14 @@ fn main() {
     println!("\n【全模式】:{}\n", words.join(" / "));
 
     //精确模式
-    words = jieba.cut("我来到北京清华大学", false, true);
+    words = jieba.cut("他来到了网易杭研大厦", false, false);
 
     println!("【精确模式】:{}\n", words.join(" / "));
+
+    //新词识别模式
+    words = jieba.cut("他来到了网易杭研大厦", false, true);
+
+    println!("【新词识别模式】:{}\n", words.join(" / "));
 
     //搜索引擎模式
     words = jieba.cut_for_search("小明硕士毕业于中国科学院计算所，后在日本京都大学深造");
